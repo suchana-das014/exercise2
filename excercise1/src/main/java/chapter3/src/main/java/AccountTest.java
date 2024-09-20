15d33f47ebaf2f63f05686b9605ff96c4a08dbbc
suchana-das014 <suchanadas541@gmail.com>
2024-09-20 13:33:02 +0600

    Add files via upload

---
+++ excercise1/src/main/java/chapter3/src/main/java/AccountTest.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class AccountTest {
+    public static void main(String[] args) {
+        
+        Account account1 = new Account("Ted Mosby",50);
+        Account account2 = new Account("Barney jane",7.53);
+        
+        System.out.println(account1.getName()+" : "+account1.getBalance());
+        System.out.println(account2.getName()+" : "+account2.getBalance());
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter account1 deposit amount : ");
+        double depositAmount = input.nextDouble();
+        account1.deposit(depositAmount);
+        System.out.println("Enter account2 deposit amount : ");
+        depositAmount = input.nextDouble();
+        account2.deposit(depositAmount);
+        
+        System.out.println(account1.getName()+" : "+account1.getBalance());
+        System.out.println(account2.getName()+" : "+account2.getBalance()); 
+    }
+}
